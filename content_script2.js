5caa5f45ede300cf9d1c0ee2f3164e25161f9f37
shupac800 <davidshuman@gmail.com>
2016-03-22 12:19:22 -0500

    some progress

--- content_script2.js
+++ content_script2.js
@@ -1,119 +1,149 @@
 "use strict";
 
-console.clear();
-console.log("Foo man T00L");
-// how to disable all links/listeners??
-// not easy without wrecking display of page!
-// angular attributes control both graphics and page linking
+var array1 = [];
+
+$(document).ready(function(){
+//  console.clear();
+  console.log("running");
 
 // disable all buttons and links:
+  $("a").css("cursor","arrow").click(false);  // maybe this belongs on all elements, not just A?
+  $("img").css("cursor","arrow").click(false);  // successfully disables clicks on cars.com car images
+  $(":input").prop("disabled",true);
 
-  // disable all other click listeners on this element
+  // disable all other click listeners
   var b = document.getElementsByTagName("*");
   var i;
   for (i = 0; i < b.length; i++) {
     var elClone = b[i].cloneNode(true);
-    b[i].parentNode.replaceChild(elClone, b[i]);
-    //b[i].style.backgroundColor = "yellow";
+    b[i].parentNode.replaceChild(elClone, b[i]);  // can't commit suicide, but can commit infanticide
   }
 
-$(document).ready(function(){
-  $("a").css("cursor","arrow").click(false);
-  // for jquery older than 1.4.3, use the below line
-  // $("a").css("cursor","arrow").click(function(){
-  //   return false;
-  // });
-
-  $(":input").prop("disabled",true);
-  // for jquery older than 1.6, use the below line
-  // $(":input").attr("disabled","disabled");
-});
-
-
-
-var todos = document.getElementsByTagName("*");
-for (var i = 0; i < todos.length; i++) {
-  var attributes = todos[i].attributes;
-  var j = attributes.length;
-  while(j--){
-    if
-       (
-        // (attributes[j].name === "href") ||
-       (attributes[j].name === "onclick") ||
-       (attributes[j].name === "ng-click") ||
-       (attributes[j].name === "ng-href"))
-       // ||
-       // (attributes[j].name === "xxxxx") ) {
-    {
-      todos[i].removeAttributeNode(attributes[j]);
+  // remove element attributes that take action upon click
+  var q = document.getElementsByTagName("*");
+  for (var i = 0; i < q.length; i++) {
+    var attributes = q[i].attributes;
+    var j = attributes.length;
+    while(j--){
+      if
+         (
+          // (attributes[j].name === "href") ||
+         (attributes[j].name === "onclick") ||
+         (attributes[j].name === "ng-click") ||
+         (attributes[j].name === "ng-href"))
+         // ||
+         // (attributes[j].name === "xxxxx") ) {
+      {
+        q[i].removeAttributeNode(attributes[j]);
+      }
     }
   }
-}
 
-/*var a = document.getElementsByTagName("a");
-for (var i = 0; i < a.length; i++) {
-  a[i].setAttribute("href","");
-  a[i].setAttribute("onclick","");
-  a[i].style.border = "2px solid #CDF"
-}*/
+  // add event listeners that allow DOM elements and their equivalents to be selected
+  var q = document.getElementsByTagName("*");
+  for (var i = 0; i < q.length; i++) {
+    q[i].addEventListener("click",function(e) {
+      e.preventDefault();  // redundant of stopPropagation?
+      e.stopPropagation();
+      var oldBgColor = e.target.style.backgroundColor;
+      popUp(e.target);
 
+      // console.log("parentNode name for this element",e.target.parentNode);
+      // console.log("parentNodeX2 name for this element",e.target.parentNode.parentNode);
+      // console.log("tag name for this element:",e.target.tagName);
+      // console.log("class list for this element:",e.target.classList);
+      // console.log("index for this element",$(this).index());
+      // console.log("attributes list for this element:",e.target.attributes);
 
-
-/*  var b = document.getElementsByClassName("result-price");
-  for (var i=0; i<b.length;i++) {
-
-    b[i].addEventListener("mouseover",function(e){
-      e.target.style.border = "2px solid blue";
-    });
-    b[i].addEventListener("mouseout",function(e){
-      e.target.style.border = "none";
-    });
-    b[i].addEventListener("click",function(e){
-      console.log(e.target.innerHTML);
-    });
-  }*/
-
-var q = document.getElementsByTagName("*");
-for (var i = 0; i < q.length; i++) {
-  q[i].addEventListener("click",function(e){
-    e.stopPropagation();
-    //alert(e.target.innerHTML);
-    var oldBgColor = e.target.style.backgroundColor;
-    e.target.style.backgroundColor = "yellow";
-
-    // console.log("parentNode name for this element",e.target.parentNode);
-    // console.log("parentNodeX2 name for this element",e.target.parentNode.parentNode);
-    console.log("tag name for this element:",e.target.tagName);
-    console.log("class list for this element:",e.target.classList);
-    //console.log("index for this element",$(this).index());
-    //console.log("attributes list for this element:",e.target.attributes);
-
-   // define the problem
-   // need to select all elements that occupy same position in DOM
-   // obviously, same tag
-   // same class, if tag has class
-   // same parent class
-   // same nth child of parent (NO -- can't rely on that)
-   // this is not a trivial task when the DOM structure is complicated!
-   // maybe the thing to do is go through the whole DOM when you rewrite it
-   // and when rewriting a tag, assign it a unique ID representing its DOM identity
-   // e.g. 
-
-    var tag = e.target.tagName;
-    var clist = e.target.classList;
-    var classStr = "";
-    clist.forEach((thisClass,idx) => {
-      classStr += "." + thisClass;
-    })
-    var z = `${tag}${classStr}`;
-    //z = "a.result-ymmt-link.ng-binding";
-    $(z).each((idx,m) => {
-      $(m).css("background-color","#3CE");
-    });
-    e.target.addEventListener("mouseout",function(e) {
-      $(z).each((idx,m) => {
-        $(m).css("background-color",oldBgColor);
+      var tag = e.target.tagName;
+      var clist = e.target.classList;
+      var index = $(e.target).index();
+      var classStr = "";
+      clist.forEach((thisClass,idx) => {
+        classStr += "." + thisClass;
+      })
+      var z = `${tag}${classStr}:nth-child(${index + 1})`;
+      //z = "a:nth-of-type(2)";
+      //console.log("z = ",z);
+      //console.log("selects " + $(z).length + " elements");
+      document.addEventListener("keypress", function(e){processKeypress(e,z);});
+      e.target.addEventListener("mouseout", function(e) {
+        console.log("got mouseout");
+        killPopUp();
+        // cancel keyup listener on mouseout
+        document.removeEventListener("keypress", processKeypress);
+        selectorAction(z,function(el) { $(el).css({"background-color":oldBgColor});} );
       });
+      selectorAction(z,function(el) { $(el).css({"background-color":"#3CE"});} );
     });
+  }
+
+});  // end document.ready()
+
+var selectorAction = function(selector,fn) {
+  $(selector).each( function(idx,element) { fn(element); } );
+};
+
+
+var processKeypress = function(e,selector){
+  console.log(e.keyCode);
+  if (e.keyCode === 108) { // lowercase "l" for less
+    console.log("less");
+  } else if (e.keyCode === 109) { // lowercase "m" for more
+    console.log("more");
+  } else if (e.keyCode === 97) { // lowercase "a" for add
+    array1 = [];
+    $(selector).each( (idx,element) => {
+      console.log("processing element",element);
+      array1.push($(element).html());
+    });
+    console.log(array1);
+    // console.log("adding ");
+
+  }
+};
+
+
+var popUp = function(element,x,y) {
+  //var rectObject = element.getBoundingClientRect();
+  //console.log("rectObject",rectObject);
+  $("body").append(`<div id='popUp'></div>`);
+  $("#popUp").css( {
+    "position":"absolute",
+    "top":y,
+    "left":x
   });
-}
+  $("#popUp").html(parseAttributes(element));
+
+};
+
+var killPopUp = function() {
+  $("#popUp").remove();
+};
+
+var parseAttributes = function(el) {
+  var namedNodeMap = el.attributes;  // .attributes returns an object with keys "0", "1", "2", ...
+  var keys = Object.keys(el.attributes);
+  var i;
+  var string = "text: " + $(el).html() + "<br>";
+  for (i = "0"; i < keys.length; i++) {
+    string += namedNodeMap[keys[i]].name + ": " + namedNodeMap[keys[i]].value + "<br>";
+  }
+  return string;
+};
+
+var doScrape = function() {
+  // function that will extract information from the DOM and put it into a JSON object
+};
+
+var uploadScrape = function() {
+  // function that will upload the JSON object to Firebase
+  // and spawn a new window showing the results
+};
+
+var selectFromAttributes = function(element) {
+  // go through each item in the pop-up menu
+  // highlight the one you're hovering over
+  // if you click an item, add that data set to the global array
+
+};
